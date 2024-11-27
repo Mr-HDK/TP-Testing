a171549bc85cd67484d2ac7fea67d3fad5828f24
Neylajb <neyla.jebri@etudiant-isi.utm.tn>
2024-11-27 15:21:20 +0100

    Ajout des captures et modification de fichier test_calcul

---
+++ TP-1/build/test/preprocess/files/test_calcul.c
@@ -0,0 +1,31 @@
+#include "src/calcul.h"
+#include "C:/Ruby30-x64/lib/ruby/gems/3.0.0/gems/ceedling-0.31.1/vendor/unity/src/unity.h"
+
+
+void test_addition(void) {
+
+    UnityAssertEqualNumber((UNITY_INT)((5)), (UNITY_INT)((addition(2, 3))), (
+
+   ((void *)0)
+
+   ), (UNITY_UINT)(5), UNITY_DISPLAY_STYLE_INT);
+
+    UnityAssertEqualNumber((UNITY_INT)((3)), (UNITY_INT)((addition(10, -7))), (
+
+   ((void *)0)
+
+   ), (UNITY_UINT)(6), UNITY_DISPLAY_STYLE_INT);
+
+
+
+    UnityAssertEqualNumber((UNITY_INT)((19)), (UNITY_INT)((addition(12, 7))), (
+
+   ((void *)0)
+
+   ), (UNITY_UINT)(8), UNITY_DISPLAY_STYLE_INT);
+
+
+
+
+
+}
