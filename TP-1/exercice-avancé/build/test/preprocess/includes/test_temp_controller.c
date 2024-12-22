e66e7b17cdff70e3095f422c0bb39318588b65c9
Hadil Affes <58861613+hadilaff@users.noreply.github.com>
2024-12-22 23:18:17 +0100

    Delete TP-1 directory

--- TP-1/exercice-avancé/build/test/preprocess/includes/test_temp_controller.c
+++
@@ -1,4 +0,0 @@
----
-- "/var/lib/gems/2.7.0/gems/ceedling-0.31.1/vendor/unity/src/unity.h"
-- src/temp_controller.h
-- build/test/mocks/mock_temp_sensor.h
--- TP-1/exercice-avancé/src/temp_sensor.h
+++
@@ -1,6 +0,0 @@
-#ifndef TEMP_SENSOR_H
-#define TEMP_SENSOR_H
-
-int read_temperature_sensor(void);
-
-#endif 
