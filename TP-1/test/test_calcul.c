dcef2a3b6eec07cc591cdd226c2aa062a408c297
devopsYGO <124690320+devopsYGO@users.noreply.github.com>
2024-12-23 15:56:02 +0100

    added a comment to test_calcul.c

--- TP-1/test/test_calcul.c
+++ TP-1/test/test_calcul.c
@@ -4,5 +4,5 @@
 void test_addition(void) {
     TEST_ASSERT_EQUAL_INT(5, addition(2, 3));
     TEST_ASSERT_EQUAL_INT(17, addition(10, 7));
-}
+    // adding a comment to do another comment still for testing purposes}
 
