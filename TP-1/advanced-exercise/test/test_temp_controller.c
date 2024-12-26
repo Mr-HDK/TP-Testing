bab76acbc9095acd3e3c668928841054c901f11b
balkissnoun <160529737+balkissnoun@users.noreply.github.com>
2024-12-26 11:51:50 +0100

    Update test_temp_controller.c

--- TP-1/advanced-exercise/test/test_temp_controller.c
+++ TP-1/advanced-exercise/test/test_temp_controller.c
@@ -2,59 +2,39 @@
 #include "temp_controller.h"
 #include "mock_temp_sensor.h"
 
-
-#define TEMP_TOO_HOT 80
-#define TEMP_TOO_COLD 0
-#define TEMP_SENSOR_ERROR_HIGH 126
-#define TEMP_SENSOR_ERROR_LOW -41
+#define TEMP_TOO_HOT 30
+#define TEMP_TOO_COLD 15
 
 void setUp(void) {
-    
     mock_temp_sensor_Init();
 }
 
 void tearDown(void) {
-    
     mock_temp_sensor_Verify();
     mock_temp_sensor_Destroy();
 }
 
-
 void test_check_temperature_should_return_1_when_too_hot(void) {
-    read_temperature_sensor_ExpectAndReturn(85); 
+    read_temperature_sensor_ExpectAndReturn(31); 
     TEST_ASSERT_EQUAL(1, check_temperature());
 }
 
-
 void test_check_temperature_should_return_minus_1_when_too_cold(void) {
-    read_temperature_sensor_ExpectAndReturn(-10); 
+    read_temperature_sensor_ExpectAndReturn(14); 
     TEST_ASSERT_EQUAL(-1, check_temperature());
 }
 
-
 void test_check_temperature_should_return_0_when_normal(void) {
-    read_temperature_sensor_ExpectAndReturn(25); 
+    read_temperature_sensor_ExpectAndReturn(22); 
     TEST_ASSERT_EQUAL(0, check_temperature());
 }
 
-
 void test_check_temperature_should_handle_exact_limits(void) {
-    
-    read_temperature_sensor_ExpectAndReturn(TEMP_TOO_COLD);
-    TEST_ASSERT_EQUAL(-1, check_temperature());
+    // Test lower limit
+    read_temperature_sensor_ExpectAndReturn(15);
+    TEST_ASSERT_EQUAL(0, check_temperature());
 
-   
-    read_temperature_sensor_ExpectAndReturn(TEMP_TOO_HOT);
-    TEST_ASSERT_EQUAL(1, check_temperature());
-}
-
-
-void test_check_temperature_should_return_error_for_sensor_failure(void) {
-    
-    read_temperature_sensor_ExpectAndReturn(TEMP_SENSOR_ERROR_LOW);
-    TEST_ASSERT_EQUAL(-1, check_temperature());
-
-   
-    read_temperature_sensor_ExpectAndReturn(TEMP_SENSOR_ERROR_HIGH);
-    TEST_ASSERT_EQUAL(1, check_temperature());
+    // Test upper limit
+    read_temperature_sensor_ExpectAndReturn(30);
+    TEST_ASSERT_EQUAL(0, check_temperature());
 }
