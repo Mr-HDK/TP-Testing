06c241d2c36d83fef47e77fc1b525575cb3d5170
RawaaHamdi <123324780+RawaaHamdi@users.noreply.github.com>
2024-12-26 18:51:18 +0100

    Update temp_controller.c

--- TP-1/advanced-exercise/src/temp_controller.c
+++ TP-1/advanced-exercise/src/temp_controller.c
@@ -4,12 +4,15 @@
 int check_temperature(void) {
     int temperature = read_temperature_sensor();
 
+    if (temperature < -40 || temperature > 125) {
+        return -2; // Échec du capteur
+    }
+
     if (temperature > 30) {
         return 1; // Trop chaud
     } else if (temperature < 15) {
         return -1; // Trop froid
     } else {
-       return 0; // Température normale
+        return 0; // Température normale
     }
 }
-
